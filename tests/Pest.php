22de75bd3dc562ced84317828e2c89e647895c8b
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-09-24 19:42:14 +0800

    fix: Correct test case extension to include 'Integration' alongside 'Feature'

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,7 +13,8 @@ use Hibla\EventLoop\EventLoop;
 |
 */
 
-pest()->extend(Tests\TestCase::class)->in('Feature', 'Unit');
+pest()->extend(Tests\TestCase::class)->in('Feature', 'Integration');
+pest()->extend(Tests\TestCase::class)->in('Unit');
 
 beforeEach(function () {
     EventLoop::reset();
