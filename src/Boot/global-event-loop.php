8834fdb1c556e279c00064bbaae32ca3bf40a23b
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-09-25 22:28:38 +0800

    refactor: Move global-event-loop.php to Boot directory and remove old test file

--- src/Boot/global-event-loop.php
+++ src/Boot/global-event-loop.php
@@ -6,16 +6,16 @@ use Rcalicdan\Defer\Defer;
 use Rcalicdan\Defer\Handlers\SignalRegistryHandler;
 
 $dotenvPaths = [
-    __DIR__.'/../',
-    __DIR__.'/../../',
-    __DIR__.'/../../../',
-    __DIR__.'/../../../../',
-    __DIR__.'/',
+    __DIR__ . '/../',
+    __DIR__ . '/../../',
+    __DIR__ . '/../../../',
+    __DIR__ . '/../../../../',
+    __DIR__ . '/',
 ];
 
 $dotenv = null;
 foreach ($dotenvPaths as $path) {
-    if (file_exists($path.'.env') || file_exists($path.'.env.example')) {
+    if (file_exists($path . '.env') || file_exists($path . '.env.example')) {
         $dotenv = Dotenv::createImmutable($path);
         break;
     }
@@ -29,19 +29,26 @@ function setupGracefulShutdown(EventLoop $eventLoop): void
     $signalHandler = new SignalRegistryHandler(function () use ($eventLoop) {
         if ($eventLoop->isRunning()) {
             $eventLoop->stop();
+            $eventLoop->reset();
         }
     });
-    
+
     $signalHandler->register();
-    
+
     register_shutdown_function(function () use ($eventLoop) {
         if ($eventLoop->isRunning()) {
             $eventLoop->stop();
+            $eventLoop->reset();
         }
     });
 }
 
-$enableEventLoop = filter_var($_ENV['ENABLE_GLOBAL_EVENT_LOOP'] ?? (getenv('ENABLE_GLOBAL_EVENT_LOOP') !== false ? getenv('ENABLE_GLOBAL_EVENT_LOOP') : 'true'), FILTER_VALIDATE_BOOLEAN);
+$enableEventLoop = filter_var(
+    getenv('ENABLE_GLOBAL_EVENT_LOOP') !== false
+        ? getenv('ENABLE_GLOBAL_EVENT_LOOP')
+        : ($_ENV['ENABLE_GLOBAL_EVENT_LOOP'] ?? 'true'),
+    FILTER_VALIDATE_BOOLEAN
+);
 
 if ($enableEventLoop) {
     Defer::global(function () {
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Hibla\EventLoop\Loop;
-
-require 'vendor/autoload.php';
-
-Loop::addPeriodicTimer(1, function () {
-    echo 'tick' . PHP_EOL;
-});
